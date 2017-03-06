270ffb6febe8cc5454146e8629350c87f706d328
ncc <ncc>
2017-03-06 09:51:35 -0800

    Added Customer class

---
+++ BankLab/BankLab/Customer.cpp
@@ -0,0 +1,16 @@
+#include "Customer.h"
+
+
+
+Customer::Customer(std::string firstName, std::string lastName) :_firstName(firstName), _lastName(lastName)
+{
+}
+
+Customer::~Customer()
+{
+}
+
+std::string Customer::getFullName()
+{
+	return _lastName + " , " + _firstName;
+}
