e0b326a0332eac7af9b2863b84b99bf100163b90
ncc <ncc>
2017-03-08 09:24:40 -0800

    UpdatedMain

---
+++ BankLab/BankLab/Bank.cpp
@@ -0,0 +1,31 @@
+#include "Bank.h"
+
+Bank::Bank(std::string name) : _name(name)
+{
+}
+
+Bank::~Bank()
+{
+}
+
+std::string Bank::GetName()
+{
+	return _name;
+}
+
+std::vector<Account> Bank::GetAccounts()
+{
+	return _accounts;
+}
+
+bool Bank::OpenAccount(Account account, int startingPennies)
+{
+	account.Deposit(startingPennies);
+	_accounts.push_back(account);
+	return true;
+}
+
+void Bank::CreateAccount(Account newAccount)
+{
+	_accounts.push_back(newAccount);
+}
--- BankLab/BankLab/Main.cpp
+++ BankLab/BankLab/Main.cpp
@@ -1,8 +1,72 @@
+#include "Bank.h"
+#include <iostream>
+using namespace std;
+Bank bank = Bank("Awesome bank");
 
+
+void DisplayMenu();
+void AddAccountMenu();
+void TransactMenu();
 
 int main()
 {
 
+	DisplayMenu();
+
+
+	system("pause");
 
 	return 0;
+}
+
+
+void DisplayMenu()
+{
+	system("cls");
+	cout << "Welcome to " << bank.GetName() << endl; 
+	cout << "1) Add Account " << endl; 
+	cout << "2) Deposit" << endl; 
+	cout << "3) Withdraw" << endl; 
+	cout << "What would you like to do?" << endl; 
+	
+	int choice;
+	cin >> choice;
+
+	switch (choice)
+	{
+	case 1: AddAccountMenu(); break;
+	case 2: TransactMenu(); break;
+	case 3: 
+	default: break;
+	
+	}
+
+}
+
+
+void AddAccountMenu()
+{
+	cout << "customer first name: ";
+	string firstName;
+	cin >> firstName;
+	
+	cout << "customer las name: ";
+	string lastName;
+	cin >> lastName;
+
+
+	cout << "Account Number: ";
+	int accountNumber;
+	cin >> accountNumber;
+
+	Customer customer = Customer(firstName, lastName);
+	Account newAccount = Account(customer, accountNumber);
+	bank.CreateAccount(newAccount);
+
+}
+
+
+void TransactMenu()
+{
+
 }
